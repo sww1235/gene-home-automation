dc5c10ac6982404fb9509ba36aa628c8c78a69d8
sww1235 <sww1235@gmail.com>
2016-12-27 18:20:08 -0700

    removed lint

--- Code/mytime.py
+++ Code/mytime.py
@@ -1,42 +1,61 @@
+#!/usr/bin/python
+
+"""
+A simple timer class.
+
+Stolen from David Thompson of Desert-home.com
+"""
 import time
 
+
 class timer:
- _things = []
+    """A timer."""
 
- def __init__(self, callback, seconds=1, minutes=0, hours=0):
-  interval = (hours*60*60) + (minutes*60) + seconds
-  actionTime = time.time() + interval
-  self._things.append({"callback":callback,"interval":interval,"actionTime":actionTime})
+    _things = []
 
- def tick(self):
-  now = time.time()
-  for i in self._things:
-   if i["callback"] == None:
-    continue
-   if now >= i["actionTime"]:
-    i["callback"]()
-    i["actionTime"] += i["interval"]
+    def __init__(self, callback, seconds=1, minutes=0, hours=0):
+        """Initilize timer class."""
+        interval = (hours*60*60) + (minutes*60) + seconds
+        actionTime = time.time() + interval
+        self._things.append({"callback": callback, "interval": interval,
+                             "actionTime": actionTime})
+
+    def tick(self):
+        """tock."""
+        now = time.time()
+        for i in self._things:
+            if i["callback"] is None:
+                continue
+            if now >= i["actionTime"]:
+                i["callback"]()
+                i["actionTime"] += i["interval"]
+
 
 checkTimer = timer(None)
 
-''' This is code to test and illustrate the usage '''
-def printSecond():
- print "second"
 
-def printTwoSeconds():
- print "twoseconds"
-
-def printMinute():
- print "minute"
-
+# This is code to test and illustrate the usage
 if __name__ == "__main__":
- # First create any timers you need
- oneSecond = timer(printSecond, seconds=1)
- twoSeconds = timer(printTwoSeconds, seconds=2)
- minute = timer(printMinute, minutes=1)
 
- # now once in a while call tick to let them happen
- while True:
-  checkTimer.tick()
-  # a sleep lets the cpu have a rest to do other things.
-  time.sleep(0.5)
+    def printSecond():
+        """Test function."""
+        print("second")
+
+    def printTwoSeconds():
+        """Test function."""
+        print("twoseconds")
+
+    def printMinute():
+        """Test function."""
+        print("minute")
+
+    # First create any timers you need
+    oneSecond = timer(printSecond, seconds=1)
+    twoSeconds = timer(printTwoSeconds, seconds=2)
+    minute = timer(printMinute, minutes=1)
+
+    # now once in a while call tick to let them happen
+    while True:
+        checkTimer.tick()
+        # a sleep lets the cpu have a rest to do other things.
+        time.sleep(0.5)
